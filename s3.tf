362a7ff499ea9f7a0d66ca472c3597d4f105417c
gopalagabbita”git config --global user.name gopalagabbita” <gopala.gabbita@gmail.com>
2024-01-14 13:39:34 -0500

    added s3 bucket

--- s3.tf
+++ s3.tf
@@ -17,3 +17,12 @@ resource "aws_s3_bucket" "ggbucketxyz002" {
   }
   depends_on = [aws_s3_bucket.ggbucketxyz001]
 }
+
+resource "aws_s3_bucket" "ggbucketxyz003" {
+  bucket = "ggbucketxyz003"
+  tags = {
+    Name        = "${var.vpc_name}-ggbucketxyz003"
+    Environment = var.environment
+  }
+  depends_on = [aws_s3_bucket.ggbucketxyz001]
+}
